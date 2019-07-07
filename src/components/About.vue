3923099fa502d4ab09e031896ef61ec1bfe28d87
harry2491 <hharwinderkaur@unomaha.edu>
2019-07-07 12:51:52 -0500

    Commiting the Vue App

---
+++ src/components/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="about">
+    <h1>HELLO {{ their_name }}, WELCOME TO THE ABOUT PAGE!</h1>
+    <p>This page is about the skills listed by the user possess. List all the skills you have here.</p>
+  </div>
+</template>
+
+<script>
+export default {  
+  name: 'Skills',
+ data(){
+   return {
+       their_name: this.$route.params.name
+   }
+ }
+}
+
+</script>
---
+++ src/components/Skills.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="hello">
+   <div class ="holder">
+     <form @submit.prevent= "addSkill">
+     <input type = "text" placeholder="Enter list of skills you possess:" v-model="skill" v-validate="'min:5'" name="skill">
+     
+     <transition name="alert-in" enter-active-class="animated slideInUp" leave-active-class="animated flipOutX">
+     <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+     </transition>
+
+     </form>
+     <ul>
+       <transition-group name="list" enter-active-class="animated bounceInRight" leave-active-class="animated bounceOutLeft">
+       <li v-for="(data, index) in skills" :key='index'>
+         {{ data.skill }}
+         <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
+         </li>
+      </transition-group>
+     </ul>
+      <p> 
+        These are the list of skills you possess.</p> 
+     </div>
+  </div>
+</template>
+
+<script>
+export default {  
+  name: 'Skills',
+ data(){
+   return {
+
+     skill: '',
+     skills: [
+       {"skill": "Vue.js"},
+       {"skill": "Front-end Developer"}
+     ]
+   }
+ },
+ methods : {
+   addSkill() {
+     this.$validator.validateAll().then((result) => {
+       if (result) {
+         this.skills.push({skill: this.skill})
+         this.skill = '';
+       } else {
+console.log('Not Valid');
+       }})
+   },
+   remove(id) {
+     this.skills.splice(id,1);
+   }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+@import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+.holder {
+    background: #f0e9e9;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+  }
+  
+  ul li {
+    padding: 20px;
+    font-size: 1.6em;
+    background-color: #E0EDF4;
+    border-left: 5px solid #3EB3F6;
+    margin-bottom: 2px;
+    color: #3E5252;
+  }
+
+  p {
+    text-align:center;
+    padding: 20px 0;
+    color: black;
+  }
+
+  .container {
+    box-shadow: 0px 0px 40px lightgray;
+  }
+  input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+  .alert-in-enter-active {
+    animation: bounce-in .5s;
+  }
+  .alert-in-leave-active {
+    animation: bounce-in .5s reverse;
+  }
+
+  @keyframes bounce-in {
+    0% {
+      transform: scale(0);
+    }
+    50% {
+      transform: scale(1.5);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+i {
+  float:right;
+  cursor: pointer;
+}
+
+</style>
